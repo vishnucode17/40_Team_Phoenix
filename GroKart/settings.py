686d44d9f34a57709d90b1d095439831372e5c67
Phoenix-Robotics-Club <vishuvishnu1717@gmail.com>
2021-07-09 20:39:41 +0530

    second commit

--- GroKart/settings.py
+++ GroKart/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'grokartapp'
+    'grokartapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
