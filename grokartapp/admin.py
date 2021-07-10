1e79684e9a94644624f71378ce72ac17da6ac202
Phoenix-Robotics-Club <vishuvishnu1717@gmail.com>
2021-07-10 13:17:37 +0530

    admin portal redesigned

--- grokartapp/admin.py
+++ grokartapp/admin.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin): 
     list_display = ('storename','product_name','category','date_added') 
+    #search_fields = ("storename__startswith", )
     def active(self, obj): 
         return obj.is_active == 1
   
     active.boolean = True
-  
+admin.site.site_header = "GroKart Admin Portal"
+admin.site.site_title = "GroKart Admin Portal"
+admin.site.index_title = "Welcome to GroKart Admin"
 admin.site.register(Product, ProductAdmin) 
